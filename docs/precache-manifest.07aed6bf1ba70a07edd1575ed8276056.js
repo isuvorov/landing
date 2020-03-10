1090c1b4b802d9b6270c7bc168c95a6a3cdb18a3
Igor Suvorov <hi@isuvorov.com>
2020-03-10 20:51:53 +0200

    chore: release

---
+++ docs/precache-manifest.07aed6bf1ba70a07edd1575ed8276056.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f8598efc5d70e48437d1224c53f77b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69d48250bdb9817766f6",
+    "url": "/static/js/2.3c5a85fc.chunk.js"
+  },
+  {
+    "revision": "dee4bcf5d90ec803f533",
+    "url": "/static/js/main.9757c521.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "04d47f8e62894b3b80b0a2ed94e3ad2d",
+    "url": "/static/media/IMG_0658 1.04d47f8e.png"
+  },
+  {
+    "revision": "c6d89c2adbf701e9bf70af439f5a8921",
+    "url": "/static/media/IMG_2087.c6d89c2a.png"
+  },
+  {
+    "revision": "9d36379822c5aa698f4fb5a242a04e1a",
+    "url": "/static/media/IMG_2088.9d363798.png"
+  },
+  {
+    "revision": "316a03e3e56230e3d1e34c917c78acd9",
+    "url": "/static/media/IMG_2089.316a03e3.png"
+  },
+  {
+    "revision": "aa63510eb01d5befbc90140ff86d6906",
+    "url": "/static/media/IMG_2090.aa63510e.png"
+  },
+  {
+    "revision": "26056f3cb9e67440fdef149fbaa55ecd",
+    "url": "/static/media/IMG_2091.26056f3c.png"
+  },
+  {
+    "revision": "cd09a719a4fc85a1820f281cdbf50ca6",
+    "url": "/static/media/IMG_2093.cd09a719.png"
+  },
+  {
+    "revision": "b94b740caf19e135ee8aa08f2809f71b",
+    "url": "/static/media/IMG_2094.b94b740c.png"
+  },
+  {
+    "revision": "1ee84d433d3f5efa2e23e45dabd707d7",
+    "url": "/static/media/IMG_2095.1ee84d43.png"
+  },
+  {
+    "revision": "40b0df57f51a745f48d179623b115d16",
+    "url": "/static/media/IMG_2096.40b0df57.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "db83bd23c0729d516e6ae047ee768c8d",
+    "url": "/static/media/bgImg.db83bd23.png"
+  },
+  {
+    "revision": "48b199012d897bc237fbb429ae812a71",
+    "url": "/static/media/bg_1.48b19901.jpg"
+  },
+  {
+    "revision": "14ace77f0904b5bdfd157824c53bdbb1",
+    "url": "/static/media/bg_2.14ace77f.jpg"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/static/media/team-4-800x800.23007132.jpg"
+  },
+  {
+    "revision": "b93639944656c50209147719cbcb66b9",
+    "url": "/static/media/парабола пион .b9363994.jpg"
+  },
+  {
+    "revision": "30df3e3fc057bc900ccce8b17f3eb549",
+    "url": "/static/media/парабола пион 2.30df3e3f.jpg"
+  },
+  {
+    "revision": "469aef3b3c12a11330e5e51151037997",
+    "url": "/static/media/парабола шоколад 2.469aef3b.jpg"
+  },
+  {
+    "revision": "f3ec21c055b79e460c2c3dfba9b8d9bb",
+    "url": "/static/media/парабола шоколад 3.f3ec21c0.jpg"
+  },
+  {
+    "revision": "3c03009df357268d0bc8a83240fa5acb",
+    "url": "/static/media/парабола шоколад 4.3c03009d.jpg"
+  },
+  {
+    "revision": "c62197b6a4bf3bb4607b26025bf880f7",
+    "url": "/static/media/парабола шоколад.c62197b6.jpg"
+  }
+]);
